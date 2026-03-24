6558f7bc0eb6231987c7419c97a8779113524abe
Lab05_etec <Lab05_etec@etec.com>
2026-03-24 15:07:06 -0300

    conclusao e adição da api node

---
+++ server.js
@@ -0,0 +1,24 @@
+//importação a partir de CommonJS
+// var express = require("express");
+
+import express, { response } from "express";
+import { request } from "http";
+
+const app = express();
+const porta = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Olá, Teste");
+});
+
+app.get("/usuario", (request, response) =>{
+    response.json({
+        id:1,
+        nome:"Renatao",
+        Profissao:"desempregadu"
+    })
+})
+
+app.listen(porta, (request,response) => {
+    console.log(`Servidor rodando: http://127.0.0.1:${porta}`)
+});
